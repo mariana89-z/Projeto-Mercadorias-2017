b2379e6b60ee363fa700a688f91b4edc4bef8df6
Mariana Cristhina Zeferino <mariana-cristhina@hotmail.com>
2017-03-19 23:23:23 -0300

    Mensagem de confirmação significativa

---
+++ documents/visual studio 2017/Projects/visual studio 2017Projects/visual studio 2017Projects/Program.cs
@@ -0,0 +1,15 @@
+﻿sagem de using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visual_studio_2017Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
